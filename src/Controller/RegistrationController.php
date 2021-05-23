824896203ff4d25f705a4f8bddf25ffdcc682fe5
adian <adiancatic@gmail.com>
2021-05-23 12:38:11 +0200

    ADD: Authentication pages improvements

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class RegistrationController extends AbstractController
 {
@@ -18,13 +23,22 @@ class RegistrationController extends AbstractController
      * @Route("/register", name="register")
      * @param Request $request
      * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param Security $security
+     * @param UserRepository $userRepository
+     * @param AuthenticationUtils $authenticationUtils
      * @return Response
      */
-    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder, Security $security, UserRepository $userRepository, AuthenticationUtils $authenticationUtils): Response
     {
+        if($security->getUser()) {
+            return $this->redirectToRoute("home");
+        }
+
         $form = $this
             ->createFormBuilder()
-            ->add("username")
+            ->add("username", TextType::class, [
+                "label" => "Username",
+            ])
             ->add("password", RepeatedType::class, [
                 "type" => PasswordType::class,
                 "required" => true,
@@ -40,23 +54,34 @@ class RegistrationController extends AbstractController
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted()) {
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        if($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
-            $user = new User();
-            $user->setUsername($data["username"]);
-            $user->setPassword(
-                $passwordEncoder->encodePassword($user, $data["password"])
-            );
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+            $userAlreadyExists = $userRepository->findOneBy(["username" => $data["username"]]);
 
-            return $this->redirect($this->generateUrl("app_login"));
+            if(!$userAlreadyExists) {
+                $user = new User();
+                $user->setUsername($data["username"]);
+                $user->setPassword(
+                    $passwordEncoder->encodePassword($user, $data["password"])
+                );
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+
+                return $this->redirect($this->generateUrl("home"));
+            }
+
+            // $error =
         }
 
         return $this->render('security/register.html.twig', [
-            "form" => $form->createView()
+            "form" => $form->createView(),
+            "error" => $error,
         ]);
     }
 }
