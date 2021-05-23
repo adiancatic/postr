7cd9bd6ed024f842558fc80fa1358426fdf52013
adian <adiancatic@gmail.com>
2021-05-23 13:00:41 +0200

    ADD: New registration form

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,85 +3,55 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Repository\UserRepository;
+use App\Form\RegistrationFormType;
+use App\Security\LoginAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/register", name="register")
+     * @Route("/register", name="app_register")
      * @param Request $request
      * @param UserPasswordEncoderInterface $passwordEncoder
-     * @param Security $security
-     * @param UserRepository $userRepository
-     * @param AuthenticationUtils $authenticationUtils
+     * @param GuardAuthenticatorHandler $guardHandler
+     * @param LoginAuthenticator $authenticator
      * @return Response
      */
-    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder, Security $security, UserRepository $userRepository, AuthenticationUtils $authenticationUtils): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginAuthenticator $authenticator): Response
     {
-        if($security->getUser()) {
-            return $this->redirectToRoute("home");
-        }
-
-        $form = $this
-            ->createFormBuilder()
-            ->add("username", TextType::class, [
-                "label" => "Username",
-            ])
-            ->add("password", RepeatedType::class, [
-                "type" => PasswordType::class,
-                "required" => true,
-                "first_options" => ["label" => "Password"],
-                "second_options" => ["label" => "Confirm password"],
-            ])
-            ->add("Register", SubmitType::class, [
-                "attr" => [
-                    "class" => "btn btn-primary"
-                ]
-            ])
-            ->getForm();
-
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
 
-        $error = $authenticationUtils->getLastAuthenticationError();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            // do anything else you need here, like send an email
 
-        if($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-
-            $userAlreadyExists = $userRepository->findOneBy(["username" => $data["username"]]);
-
-            if(!$userAlreadyExists) {
-                $user = new User();
-                $user->setUsername($data["username"]);
-                $user->setPassword(
-                    $passwordEncoder->encodePassword($user, $data["password"])
-                );
-
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($user);
-                $em->flush();
-
-                return $this->redirect($this->generateUrl("home"));
-            }
-
-            // $error =
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
         }
 
         return $this->render('security/register.html.twig', [
-            "form" => $form->createView(),
-            "error" => $error,
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
