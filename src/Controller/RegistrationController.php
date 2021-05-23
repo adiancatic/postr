fbbb1c785e82c4b1eb2e126d8ce53196c524fd16
adian <adiancatic@gmail.com>
2021-05-23 13:10:37 +0200

    ADD: Redirect loged-in user to home if registration page accessed

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -24,6 +24,10 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginAuthenticator $authenticator): Response
     {
+        if($this->getUser()) {
+            return $this->redirectToRoute("home");
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
